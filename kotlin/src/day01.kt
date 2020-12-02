3e9096567c7839c5e44b97aa0ca768c07a51b57b
Mari Baburina <maria.baburina@jetbrains.com>
2020-12-02 02:28:50 +0100

    day 1: part 1. got rid of toTypedArray() and used a temp variable in the while loop

--- kotlin/src/day01.kt
+++ kotlin/src/day01.kt
@@ -2,8 +2,8 @@ package day1
 
 import util.readInputDatatoList
 
-fun main(args: Array<String>) {
-    val sortedArray = readInputDatatoList("input_data/day01.txt").sorted().toTypedArray()
+fun main() {
+    val sortedArray = readInputDatatoList("input_data/day01.txt").sorted()
 
     val pair = findPairOfElements(0, sortedArray.size - 1, sortedArray, 2020)
 
@@ -14,15 +14,16 @@ fun main(args: Array<String>) {
 
 }
 
-fun findPairOfElements(l: Int, r: Int, sortedArray: Array<Int>, sum: Int): Pair<Int, Int> {
+fun findPairOfElements(l: Int, r: Int, sortedArray: List<Int>, sum: Int): Pair<Int, Int> {
     var l1 = l
     var r1 = r
 
     while (l < r) {
+        val tempSum = sortedArray[l1] + sortedArray[r1]
         when {
-            sortedArray[l1] + sortedArray[r1] == sum -> break
-            sortedArray[l1] + sortedArray[r1] < sum -> l1++
-            sortedArray[l1] + sortedArray[r1] > sum -> r1--
+            tempSum == sum -> break
+            tempSum < sum -> l1++
+            tempSum > sum -> r1--
         }
     }
     return Pair(sortedArray[l1], sortedArray[r1])
