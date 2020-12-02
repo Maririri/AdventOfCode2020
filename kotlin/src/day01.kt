d3f06c41dd29766a5ca60e790dd0e86f34ea750b
Mari Baburina <maria.baburina@jetbrains.com>
2020-12-02 04:17:40 +0100

    day 1: part 1. change the return type of findPairOfElements to nullable type + update the test

--- kotlin/src/day01.kt
+++ kotlin/src/day01.kt
@@ -5,26 +5,22 @@ import util.readInputDatatoList
 fun main() {
     val sortedArray = readInputDatatoList("input_data/day01.txt").sorted()
 
-    val pair = findPairOfElements(0, sortedArray.size - 1, sortedArray, 2020)
-
-    if (pair.first + pair.second == 2020)
-        println("Solved. The answer is " + (pair.first * pair.second))
-    else
-        println("Keep thinking")
-
+    findPairOfElements(0, sortedArray.size - 1, sortedArray, 2020)?.let {
+        println("Solved, The answer is " + (it.first * it.second))
+    } ?: error("Keep thinking")
 }
 
-fun findPairOfElements(l: Int, r: Int, sortedArray: List<Int>, sum: Int): Pair<Int, Int> {
+fun findPairOfElements(l: Int, r: Int, sortedArray: List<Int>, sum: Int): Pair<Int, Int>? {
     var l1 = l
     var r1 = r
 
     while (l1 < r1) {
         val tempSum = sortedArray[l1] + sortedArray[r1]
         when {
-            tempSum == sum -> break
+            tempSum == sum -> return Pair(sortedArray[l1], sortedArray[r1])
             tempSum < sum -> l1++
             tempSum > sum -> r1--
         }
     }
-    return Pair(sortedArray[l1], sortedArray[r1])
+    return null
 }
--- kotlin/test/test_day01.kt
+++ kotlin/test/test_day01.kt
@@ -5,10 +5,10 @@ import day1.findPairOfElements
 
 class test_day01() {
     @Test
-    fun test() {
-        val testArray = arrayOf(1721, 299)
-        val testPair = findPairOfElements(0, testArray.size - 1, testArray, 2020)
-
-        assertEquals(testPair.first + testPair.second, 2020)
+    fun checkTwoNumbers() {
+        val testArray = listOf(1721, 299)
+        findPairOfElements(0, testArray.size - 1, testArray, 2020)?.let {
+            assertEquals(it.first + it.second, 2020)
+        }
     }
 }
