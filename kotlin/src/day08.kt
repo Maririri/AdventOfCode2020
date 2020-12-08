5231488f68b317d249067f72c5829c32e9779bc4
Mari Baburina <maria.baburina@jetbrains.com>
2020-12-08 09:32:06 +0100

    day8: part 2

--- kotlin/src/day08.kt
+++ kotlin/src/day08.kt
@@ -4,31 +4,61 @@ const val fileName = "input_data/day08.txt"
 
 fun main() {
     val listOfPair = readInputToList(fileName).map {
-        transformToInstrPair(it) /*to false*/
+        transformToInstrPair(it)
     }
-    val answer1 = findAcc1(listOfPair)
+    val answer1 = findAcc1(listOfPair).first
 
     if (answer1 > 0)
         println("Solved. The answer is $answer1")
     else
         println("Keep Thinking")
+
+    val answer2 = findAcc2(listOfPair)
+
+    if (answer2 > 0)
+        println("Solved. The answer is $answer2")
+    else
+        println("Keep Thinking")
 }
 
-fun findAcc1(listOfPair: List<Pair<String, Int>>): Int {
+fun findAcc1(listOfPair: List<Pair<String, Int>>): Pair<Int, Boolean> {
     var idx = 0
-    var answer = 0
-    val visitedPositions = MutableList(listOfPair.size) { false }
+    var accum = 0
+    var hasTerm = false
+    val size = listOfPair.size
+    val visitedPositions = MutableList(size) { false }
     while (!visitedPositions[idx]) {
         visitedPositions[idx] = true
         val instr = listOfPair[idx].first
         val value = listOfPair[idx].second
         when (instr) {
             "acc" -> {
-                answer += value
+                accum += value
                 idx++
             }
             "jmp" -> idx += value
             "nop" -> idx++
+        }
+        if (idx >= size) {
+            hasTerm = true
+            break
+        }
+    }
+    return accum to hasTerm
+}
+
+fun findAcc2(listOfPair: List<Pair<String, Int>>): Int {
+    var answer = 0
+    listOfPair.forEachIndexed { idx, it ->
+        val mutableList = listOfPair.toMutableList()
+        when (it.first) {
+            "jmp" -> mutableList[idx] = "nop" to it.second
+            "nop" -> mutableList[idx] = "jmp" to it.second
+        }
+        val findAcc1 = findAcc1(mutableList)
+        if(findAcc1.second){
+            answer = findAcc1.first
+            return answer
         }
     }
     return answer
--- kotlin/test/test_day08.kt
+++ kotlin/test/test_day08.kt
@@ -2,9 +2,7 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class test_day08 {
-    @Test
-    fun test1() {
-        val testData = """nop +0
+    val testData = """nop +0
 acc +1
 jmp +4
 acc +3
@@ -13,7 +11,16 @@ acc -99
 acc +1
 jmp -4
 acc +6"""
+
+    @Test
+    fun test1() {
         val testList = testData.split("\n").map { transformToInstrPair(it) }
-        assertEquals(findAcc1(testList), 5)
+        assertEquals(findAcc1(testList).first, 5)
+    }
+
+    @Test
+    fun test2() {
+        val testList = testData.split("\n").map { transformToInstrPair(it) }
+        assertEquals(findAcc2(testList), 8)
     }
 }
